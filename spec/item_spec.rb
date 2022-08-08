663ab1138cdb8324e074a3917cf8f3afc69b8dcf
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-08 09:09:43 -0600

    Move tests to the correct file & make them pass for item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,16 @@
-a
+require './lib/item'
+
+RSpec.describe do
+  it 'exists' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    expect(item1).to be_instance_of(Item)
+  end
+
+  it 'has atttributes' do
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    expect(item2.name).to eq('Tomato')
+    expect(item2.price).to eq(0.5)
+  end
+end
